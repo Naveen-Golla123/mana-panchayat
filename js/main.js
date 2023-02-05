bf947f74d48c141b938e33c0f9aff715c04fb591
jayakrishna-miriyam <jayamiriyam123@gmail.com>
2023-02-05 09:36:41 +0530

    Butified Code & added next & previous buttons

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-
-
-let imageInputEl = document.getElementById("imageInput");
-let uploadedFile = null;
-
-imageInputEl.addEventListener('change', function (event) {
-  console.log(event);
-  uploadedFile = event.target.files[0];
-
-});
-
-
-
-
 var requestOptions = {
   method: 'GET',
   redirect: 'follow'
@@ -24,82 +10,23 @@ var postRequestOptions = {
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
-  },
-  // body: {
-  //   "id": 2,
-  //   "title": "Stock Market: మార్కెట్ల బడ్జెట్ దూకుడు.. నష్టపోయిన స్టాక్స్.. లాభపడిన స్టాక్స్ ఇవే..",
-  //   "news": "Hello",
-  //   "imgUrl": "",
-  //   "location": "pamur",
-  //   "MetaDescription": ""
-  // }
+  }
 };
 
-fetch("https://mana-panchayat-server.vercel.app/news/", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-
-    let newsTitleEl = document.getElementById("newsTitle");
-    let newContentEl = document.getElementById("newContent");
-    let imgContainerEl = document.getElementById("imgContainer");
-    let allPostsEl = document.getElementById("allPosts");
-    let holderEl = document.getElementById("holder");
-
-
-    for (let each in result) {
-
-      let divTag = document.createElement('div');
-      divTag.setAttribute("id", result[each].id);
-      divTag.classList.add("row", "border", "mt-3", "py-3");
-
-
-      let allPostsCode = `
-            
-              <div class="col-3">
-                  <img class="w-100 h-auto" src="${result[each].imgUrl}" alt="" srcset="">
-              </div>
-              <div class="col-9">
-                  <h5 class="font-weight-bold">${result[each].title}</h5>
-                  <div class="mt-3" style="max-height:50px; overflow:hidden;">
-                    <p class="" >${result[each].newsDescription}</p>
-                  </div>
-                  
-                  <div class="d-flex justify-content-between mt-3">
-                    <p class="text-primary p-0">Read More</p>
-
-                    <div class="d-flex justify-content-between">
-                      <p class="text-secondary mr-3"><i class="fa-solid fa-location-dot mr-2"></i>${result[each].location}</p>
-                      <p class="text-secondary"><i class="fa-regular fa-clock mr-2"></i>${result[each].createdOn}</p>                    
-                    </div>
-
-                  </div>
-              </div>           
-          `;
-
-      divTag.innerHTML = allPostsCode;
-      allPostsEl.appendChild(divTag);
-
-
-
-    }
-
-    // newsTitleEl.innerHTML = result[0].title;
-    // newContentEl.innerHTML = result[0].newsDescription;
-    // imgContainerEl.setAttribute("src", "https://tikkablob.blob.core.windows.net/tikka-items/Chicken_Leg.png");
-    // console.log(imgContainerEl);
-
-  }
-
-  )
-  .catch(error => console.log('error', error));
-
-
-
-
+let imageInputEl = document.getElementById("imageInput");
+let uploadedFile = null;
 let postSaveButtonEl = document.getElementById("postSaveButton");
 let titleInputEl = document.getElementById("titleInput");
 let validationAlertEl = document.getElementById("validationAlert");
 let alertMessageEl = document.getElementById("alertMessage");
+
+window.onload=function(){
+
+imageInputEl.addEventListener('change', function (event) {
+  console.log(event);
+  uploadedFile = event.target.files[0];
+
+});
 
 postSaveButtonEl.addEventListener("click", function (event) {
 
@@ -115,8 +42,7 @@ postSaveButtonEl.addEventListener("click", function (event) {
 
   let textAreaEl = document.getElementById("textArea");
 
-  console.log(textAreaEl.innerHTML);
-  console.log(titleInputEl.value);
+
 
   // let payloadBody = {
   //   'id': '',
@@ -195,4 +121,75 @@ postSaveButtonEl.addEventListener("click", function (event) {
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
-});  
+}); 
+
+}
+
+
+
+
+
+fetch("https://mana-panchayat-server.vercel.app/news/", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+
+    let newsTitleEl = document.getElementById("newsTitle");
+    let newContentEl = document.getElementById("newContent");
+    let imgContainerEl = document.getElementById("imgContainer");
+    let allPostsEl = document.getElementById("allPosts");
+    let holderEl = document.getElementById("holder");
+
+
+    for (let each in result) {
+
+      let divTag = document.createElement('div');
+      divTag.setAttribute("id", result[each].id);
+      divTag.classList.add("row", "border", "mt-3", "py-3");
+
+
+      let allPostsCode = `
+            
+              <div class="col-3">
+                  <img class="w-100 h-auto" src="${result[each].imgUrl}" alt="" srcset="">
+              </div>
+              <div class="col-9">
+                  <h5 class="font-weight-bold">${result[each].title}</h5>
+                  <div class="mt-3" style="max-height:50px; overflow:hidden;">
+                    <p class="" >${result[each].newsDescription}</p>
+                  </div>
+                  
+                  <div class="d-flex justify-content-between mt-3">
+                    <p class="text-primary p-0">Read More</p>
+
+                    <div class="d-flex justify-content-between">
+                      <p class="text-secondary mr-3"><i class="fa-solid fa-location-dot mr-2"></i>${result[each].location}</p>
+                      <p class="text-secondary"><i class="fa-regular fa-clock mr-2"></i>${result[each].createdOn}</p>                    
+                    </div>
+
+                  </div>
+              </div>           
+          `;
+
+      divTag.innerHTML = allPostsCode;
+      allPostsEl.appendChild(divTag);
+
+
+
+    }
+
+    // newsTitleEl.innerHTML = result[0].title;
+    // newContentEl.innerHTML = result[0].newsDescription;
+    // imgContainerEl.setAttribute("src", "https://tikkablob.blob.core.windows.net/tikka-items/Chicken_Leg.png");
+    // console.log(imgContainerEl);
+
+  }
+
+  )
+  .catch(error => console.log('error', error));
+
+
+
+
+
+
+ 
