ddf4b5a27948d11041883db2823f2dd0a9f4a579
jayakrishna-miriyam <jayamiriyam123@gmail.com>
2023-02-07 23:08:34 +0530

    working on Post to create news

--- js/create_newspost.js
+++ js/create_newspost.js
@@ -1,7 +1,14 @@
 
-// CK Editor Section
-let textAreaEl = document.getElementById("textArea");
 
+let postSaveButtonEl = document.getElementById("postSaveButton");
+let imageInputEl = document.getElementById("imageInput");
+let titleInputEl = document.getElementById("titleInput");
+let editor1El = document.getElementById("editor1");
+let validationAlertEl = document.getElementById("validationAlert");
+let alertMessageEl = document.getElementById("alertMessage");
+
+let uploadedFile = null;
+// CK Editor Section
 let em_string = "";
 
 CKEDITOR.replace( 'editor1' );
@@ -13,8 +20,81 @@ let contentOutput = CKEDITOR.on( 'instanceReady', function( evt )
 editor.on('change', function (e) {  
     var text =  editor.editable().getData();
     em_string = text;
-    
-    textAreaEl.innerHTML = em_string;
-    
+        
     });
 }); ;
+
+
+imageInputEl.addEventListener('change', function (event) {
+  uploadedFile = event.target.files[0];
+  // console.log(uploadedFile);
+
+});
+
+
+postSaveButtonEl.addEventListener("click", function (event) {
+
+
+  if (titleInputEl.value === "") {
+    alertMessageEl.innerHTML = "Pleas give your Title!";
+    validationAlertEl.classList.remove('d-none');
+  } else if (contentOutput === "") {
+    alertMessageEl.innerHTML = "Pleas write something on Post!";
+    validationAlertEl.classList.remove('d-none');
+
+  } else {
+    validationAlertEl.classList.add('d-none');
+
+
+
+  }
+
+//  ------------------
+
+  var myHeaders = new Headers();
+  myHeaders.append("authority", "mana-panchayat-server.vercel.app");
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("accept-language", "en-US,en;q=0.9");
+  myHeaders.append("dnt", "1");
+  myHeaders.append("origin", "null");
+  myHeaders.append("sec-ch-ua", "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"");
+  myHeaders.append("sec-ch-ua-mobile", "?1");
+  myHeaders.append("sec-ch-ua-platform", "\"Android\"");
+  myHeaders.append("sec-fetch-dest", "empty");
+  myHeaders.append("sec-fetch-mode", "no-cors");
+  myHeaders.append("sec-fetch-site", "cross-site");
+  myHeaders.append("Access-Control-Allow-Origin", "*");
+  myHeaders.append("Access-Control-Allow-Methods", "POST");
+  myHeaders.append("Access-Control-Allow-Headers", "Content-Type");
+  myHeaders.append("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36");
+  myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im5hdmVlbl9nb2xsYSIsImlhdCI6MTY3NTc1NTc2NSwiZXhwIjoxNjc1OTI4NTY1fQ.3IWdI4BtBze8qcUxDbToX10n5B5adI3DDAl-7qYJV04");
+
+  var formdata = new FormData();
+  formdata.append("file", uploadedFile.name);
+  formdata.append("title", titleInputEl.value);
+  formdata.append("news", em_string);
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: formdata,
+    redirect: 'follow',
+  };
+
+  fetch("https://mana-panchayat-server.vercel.app/news", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}); 
+
+
+
+
+
+
+
+
+
+
+
+ 
--- js/view_post.js
+++ js/view_post.js
@@ -11,7 +11,16 @@ var requestOptions = {
 let newsTitleEl = document.getElementById("newsTitle"); //News Post Title
 let newsContentEl = document.getElementById("newsContent"); //News Post Content or Story
 let imgContainerEl = document.getElementById("imgContainer"); // News Post Image
+let authorNameEl = document.getElementById("authorName"); // News Post Image
+let publishedDateEl = document.getElementById("publishedDate"); // News Post Image
 
+function dateToFormat(timeStamp){
+  let dateString = timeStamp;
+  dateString = dateString.slice(0, -5);
+  let date = new Date(dateString);
+  let formattedDate = `${("0" + date.getDate()).slice(-2)} ${(date.toLocaleString("en-US", { month: "long" }))} ${date.getFullYear()}`;
+  return formattedDate;
+}
 
 
 fetch("https://mana-panchayat-server.vercel.app/news/142", requestOptions)
@@ -21,6 +30,11 @@ fetch("https://mana-panchayat-server.vercel.app/news/142", requestOptions)
     newsTitleEl.innerHTML = result[0].title; //Assigning Title to Post
     imgContainerEl.setAttribute("src", result[0].imgUrl); // Assigning Image to Post
     newsContentEl.innerHTML = result[0].newsDescription; //Assigning Content or Story to Post
+    authorNameEl.textContent = result[0].author;
+
+    let dateFormat = dateToFormat(result[0].createdOn);
+
+    publishedDateEl.textContent = dateFormat;
 
 }
 
